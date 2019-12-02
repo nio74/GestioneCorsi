e540e7d0e59b05e0afd06b4f137909b41d1c8677
nio74 <mpaolo74@gmail.com>
2019-12-02 22:15:26 +0100

    Primo invio

---
+++ src/it/nio74/GestioneCorsiController.java
@@ -0,0 +1,34 @@
+/**
+ * Sample Skeleton for 'GestioneCorsi.fxml' Controller Class
+ */
+
+package it.nio74;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class GestioneCorsiController {
+
+    @FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML
+    void doCalcolaStatCorsi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void doCercaCorsi(ActionEvent event) {
+
+    }
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+
+    }
+}
